06d42b7066cc3df8d1495187ad96984fdf078ba9
Maeve Crossan <maevecrossan@gmail.com>
2024-06-25 20:07:05 +0000

    Update division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,7 +94,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "/") {
-        return [operand1 % operand2, "division"];
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
